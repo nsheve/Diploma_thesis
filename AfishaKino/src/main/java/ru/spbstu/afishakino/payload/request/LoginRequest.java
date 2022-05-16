c40a7d8ebe7fdcd1e6b75e7fdcea1f41c806b702
nsheve <nsheve@inbox.ru>
2022-05-16 13:14:48 +0300

    adding-payload-for-spring-security

---
+++ AfishaKino/src/main/java/ru/spbstu/afishakino/payload/request/LoginRequest.java
@@ -0,0 +1,12 @@
+package ru.spbstu.afishakino.payload.request;
+
+import lombok.Data;
+import javax.validation.constraints.NotBlank;
+
+@Data
+public class LoginRequest {
+    @NotBlank
+    private String username;
+    @NotBlank
+    private String password;
+}
---
+++ AfishaKino/src/main/java/ru/spbstu/afishakino/payload/response/MessageResponse.java
@@ -0,0 +1,10 @@
+package ru.spbstu.afishakino.payload.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class MessageResponse {
+    private String message;
+}
